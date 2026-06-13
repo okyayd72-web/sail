10bb4599c70fe14f3d3da8fd58061002d8617e7f
Deniz <okyayd72@gmail.com>
2026-06-13 18:39:04 -0400

    Fix limiter storage crash, revert to memory

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
-    key_func=get_remote_address,
-    storage_uri=os.getenv('DATABASE_URL','memory://'),
+    key_func=get_remote_address
     )
 
 def create_app():
